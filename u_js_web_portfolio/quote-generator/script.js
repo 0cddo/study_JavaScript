647800527225caef1fd8b895c2b5320fb6e0f78c
0cddo <0.c.dddo@gmail.com>
2022-02-16 22:58:06 +0900

    html과 JavaScript 연동, long-quote 동적 스타일링

--- u_js_web_portfolio/quote-generator/script.js
+++ u_js_web_portfolio/quote-generator/script.js
@@ -1,15 +1,34 @@
-// let apiQuotes = [];
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
   //  pick a random quote from apiQuotes array
   const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-  // console.log(localQuotes.length);
-  console.log(localQuotes.indexOf(quote));
-  console.log(quote);
+
+  // Check if Author is blank and replace it with 'Unknow'
+  if (!quote.author) {
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  // Check Quote length to determine styling
+  if (quote.text.length > 50) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  quoteText.textContent = quote.text;
 }
+
 // Get Quotes From API
-/* async function getQuotes() {
+async function getQuotes() {
   const apiUrl = 'https://type.fit/api/quotes';
   try {
     const response = await fetch(apiUrl);
@@ -19,10 +38,9 @@ function newQuote() {
   } catch (error) {
     // Catch Error Here
   }
-} */
+}
+
+// Tweet Quote(트위터 버튼 함수)
 
 // on Load
-// getQuotes();
-
-// locally use data
-newQuote();
+getQuotes();
