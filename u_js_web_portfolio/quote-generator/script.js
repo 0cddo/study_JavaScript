780b89e0b763c0173494744ab4f254ae1a00c09e
0cddo <0.c.dddo@gmail.com>
2022-02-25 23:56:07 +0900

    로딩 기능 생성

--- u_js_web_portfolio/quote-generator/script.js
+++ u_js_web_portfolio/quote-generator/script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
 // Show New Quote
 function newQuote() {
+  // new Quote 버튼을 누를 때 getQuotes 함수가 무시되고 newQuote함수만 실행되기 때문에 버튼을 두세번 누르면 getQuotes 함수가 무시되서, 기능을 확실하게 하기 위해 loading 함수를 추가로 넣는다
+  loading();
+
   //  pick a random quote from apiQuotes array
   const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
 
@@ -24,11 +38,14 @@ function newQuote() {
   } else {
     quoteText.classList.remove('long-quote');
   }
+  // Set Quote, Hide Loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get Quotes From API
 async function getQuotes() {
+  loading();
   const apiUrl = 'https://type.fit/api/quotes';
   try {
     const response = await fetch(apiUrl);
